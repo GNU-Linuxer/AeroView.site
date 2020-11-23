2fa57212b0f8eef60a3d56c75020031f9a074b0c
David Xie <xdkdavid@gmail.com>
2020-11-23 10:13:47 -0800

    fix dependency for reactstrap and react-fontawesome

---
+++ src/toolbar.js
@@ -0,0 +1,3 @@
+import React, {useState} from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
