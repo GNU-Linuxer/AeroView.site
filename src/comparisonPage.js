93581cc55b8edbb8cd03962d19302fd44eb695e8
David Xie <xdkdavid@gmail.com>
2020-12-06 15:18:05 -0800

    prelimiary solution of grid table

--- src/comparisonPage.js
+++ src/comparisonPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // Load custom style sheet
 import './css/site-elements.css';
@@ -6,7 +6,6 @@ import './css/comparison.css';
 
 // Reactstrap depends on bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { faTruckPickup } from '@fortawesome/free-solid-svg-icons';
 
 // import { BrowserRouter, Route, Switch, Link, NavLink, useParams } from 'react-router-dom';
 //console.log(RenderComparisonContent);
@@ -14,209 +13,140 @@ import { faTruckPickup } from '@fortawesome/free-solid-svg-icons';
 let excludedMeta = ['make', 'model', 'icao-pic'];
 
 export function ComparisonPage(props) {
+    // displayPlane is an array of icao strings that controls which airplane (and the order) displaying in comparison
+    // Load favorite planes (checked the heart button) to populate comparison
+    //const [displayPlane, setDisplayPlane] = useState(props.favoritePlanes);
+    const [displayPlane, setDisplayPlane] = useState(['bcs3', 'a20n', 'mc23']);
+    // position (start from 0) determines which column would show this icao airplane
+    const updateFavoritePlane = function (icao, position) {
+        let updatedFavoritePlanes = [...displayPlane] // Array copy
+        //console.log(icao + ' ' + operation);
+        updatedFavoritePlanes[position] = icao;
+        console.log(updatedFavoritePlanes);
+        setDisplayPlane(updatedFavoritePlanes);
+    }
 
-  return (
-    // <!-- Second heading, I don't think that this should apply the style in main content -->
-    <div>
-      <main className="page-content">
+    return (
+        <main className="page-content">
 
-        <div className="dropdown">
-          <DropDownMenus favPlanes={props.favoritePlanes} airplaneData={props.airplaneData} />
-        </div>
+            <div className="dropdown">
+                <DropDownMenus favPlanes={props.favoritePlanes} airplaneData={props.airplaneData}/>
+            </div>
 
-        <div className="chart-content">
-          <RenderGrid airplaneDisplayMetaName={props.airplaneDisplayMetaName} airplaneData={props.airplaneData} favoritePlanes={props.favoritePlanes} />
-        </div>
-      </main>
-    </div>
-  )
+            <RenderGrid // Continue passing data down to child components
+                airplaneDisplayMetaName={props.airplaneDisplayMetaName}
+                airplaneData={props.airplaneData}
+                displayPlane={displayPlane}
+
+                planeRating={props.planeRating}
+                updateRatingFn={props.updateRatingFn}
+
+                favoritePlanes={props.favoritePlanes}
+                updateFavoriteFn={props.updateFavoriteFn}/>
+        </main>
+    )
 }
 
 function DropDownMenus(props) {
-  let dropDownComponent
-  let planeName;
+    let dropDownComponent
+    let planeName;
 
-  const dropDownClick = () => {
-    // Execute behavior when clicking on dropdown menu items
-    console.log('test');
-  }
+    const dropDownClick = () => {
+        // Execute behavior when clicking on dropdown menu items
+        console.log('test');
+    }
 
-  let dropDowns = props.favPlanes.map((favPlaneItem) => {
-    props.airplaneData.map((planeItem) => {
-      if (favPlaneItem === planeItem.icao.toLowerCase()) {
-        planeName = planeItem.make + ' ' + planeItem.model;
-      }
-      dropDownComponent = <span><button className="btn btn-secondary dropdown-toggle" key={planeName}>{planeName}</button></span>
+    let dropDowns = props.favPlanes.map((favPlaneItem) => {
+        props.airplaneData.map((planeItem) => {
+            if (favPlaneItem === planeItem.icao.toLowerCase()) {
+                planeName = planeItem.make + ' ' + planeItem.model;
+            }
+            dropDownComponent = <span><button className="btn btn-secondary dropdown-toggle"
+                                              key={planeName}>{planeName}</button></span>
+        })
+        return dropDownComponent;
     })
-    return dropDownComponent;
-  })
 
-  let dropDownMenuItems = Object.keys(props.airplaneData).map((planeItem) => { // Not sure how to append these items to the `dropDowns` elements
-    let drops = <a className='dropdown-item' href='#' onClick={dropDownClick} key={planeItem}>{props.airplaneData[planeItem].make + ' ' + props.airplaneData[planeItem].model}</a>
-    return drops;
-  })
+    let dropDownMenuItems = Object.keys(props.airplaneData).map((planeItem) => { // Not sure how to append these items to the `dropDowns` elements
+        let drops = <a className='dropdown-item' href='#' onClick={dropDownClick}
+                       key={planeItem}>{props.airplaneData[planeItem].make + ' ' + props.airplaneData[planeItem].model}</a>
+        return drops;
+    })
 
-  return (
-    <div>
-      {dropDowns}
-    </div>
-  )
+    return (
+        <div>
+            {dropDowns}
+        </div>
+    )
 }
 
 function RenderGrid(props) {
-  return (
-    <div>
-      <RenderHeaderColumn airplaneDisplayMetaName={props.airplaneDisplayMetaName} />
-      <RenderPlaneContent airplaneData={props.airplaneData} airplaneDisplayMetaName={props.airplaneDisplayMetaName} favoritePlanes={props.favoritePlanes} />
-    </div>
-  )
-}
-
-function RenderHeaderColumn(props) {
-  let headerColumn = Object.keys(props.airplaneDisplayMetaName).map((metaItem) => {
-    if (!excludedMeta.includes(metaItem)) {
-      let headers = <p className="chart-cell header-column" key={metaItem}>{props.airplaneDisplayMetaName[metaItem]}</p>
-      return headers;
+    let planeContentElems = [];
+    let limit = 5;
+    // Restrict how many columns to display based on available browser width
+    for (let i = 0; i < limit; i = i + 1) {
+        //console.log(props.displayPlane[i]);
+        let oneICAO = props.displayPlane[i];
+        if (oneICAO === undefined) {
+            // Populate 18 empty cells if no plane is present.
+            for (let i = 1; i <= 18; i = i + 1) {
+                planeContentElems.push(<p className="chart-cell column">&nbsp;</p>);
+            }
+        } else {
+            // Find the plane object that matches oneICAO
+            for (let onePlane of props.airplaneData) {
+                if (onePlane['icao-pic'].toLowerCase() === oneICAO) {
+                    //console.log('checkpoint');
+                    planeContentElems.push(<p className="chart-cell column">{onePlane['make'] + ' ' + onePlane['model']}</p>);
+                    planeContentElems.push(<img className="chart-cell column comparison-tile-image"
+                        src={"./plane-thumbnail/" + onePlane['icao-pic'].toLowerCase() + ".jpg"}
+                        alt={"Picture of " + onePlane['make'] + " " + onePlane['model'] + " in " + onePlane['make'] + " livery"}/>);
+                    // Conditionally populate the planeContentElems
+                    for (let oneMeta of Object.keys(onePlane)) {
+                        if (!excludedMeta.includes(oneMeta)) {
+                            planeContentElems.push(<p className="chart-cell column">{onePlane[oneMeta]}</p>);
+                        }
+                    }
+                }
+            }
+        }
     }
-  })
-
-  return (
-    <div>
-      <p className="chart-cell header-column">Name</p>
-      <p className="chart-cell header-column">Picture</p>
-      {headerColumn}
-    </div>
-  )
+    let headerColumnElems = [];
+    headerColumnElems.push(<p className="chart-cell header-column">Name</p>);
+    headerColumnElems.push(<p className="chart-cell header-column">Picture</p>);
+    for (let oneMeta of Object.keys(props.airplaneDisplayMetaName)) {
+        if (!excludedMeta.includes(oneMeta)) {
+            headerColumnElems.push(<p className="chart-cell header-column"
+                                      key={oneMeta}>{props.airplaneDisplayMetaName[oneMeta]}</p>);
+        }
+    }
+    return (
+        <div className="chart-content">
+            {headerColumnElems}
+            {planeContentElems}
+        </div>
+    )
 }
+
+// function RenderHeaderColumn(props) {
+//     let headerColumn = Object.keys(props.airplaneDisplayMetaName).map((metaItem) => {
+//         if (!excludedMeta.includes(metaItem)) {
+//             let headers = <p className="chart-cell header-column"
+//                              key={metaItem}>{props.airplaneDisplayMetaName[metaItem]}</p>
+//             return headers;
+//         }
+//     })
+//
+//     return (
+//         <div>
+//             <p className="chart-cell header-column">Name</p>
+//             <p className="chart-cell header-column">Picture</p>
+//             {headerColumn}
+//         </div>
+//     )
+// }
 
 // let planeMetaData = Object.keys(props.airplaneDisplayMetaName).map((metaItem => {
 //   if (!excludedMeta.includes(metaItem)) {
 //     //console.log(metaItem);
 //   }
-
-function RenderPlaneContent(props) {
-
-  //let metadataItems = <p className="chart-cell column" key={favPlane}>{props.airplaneData[i].metaItem}</p>
-
-  // for (let favPlane of props.favoritePlanes) {
-  //   for (let meta of Object.keys(props.airplaneData[i])) {
-  //     if (!excludedMeta.includes(meta)) {
-  //       if (props.airplaneData[i].icao.toLowerCase() === favPlane) {
-  //         console.log(meta);
-  //         let metadataItems = <p className="chart-cell column" key={favPlane}>{props.airplaneData[i].meta}</p>
-  //         return metadataItems;
-  //       }
-  //     }
-  //   }
-  // })
-
-  let metaData = Object.keys(props.airplaneDisplayMetaName).map((metaItem) => {
-    if (!excludedMeta.includes(metaItem)) {
-      return metaItem;
-    }
-  })
-  //console.log(planeMetaData);
-
-  return (
-    <div>
-      <RenderPlaneName airplaneDisplayMetaName={props.airplaneDisplayMetaName} airplaneData={props.airplaneData} favoritePlanes={props.favoritePlanes} />
-      <RenderPlaneImage airplaneDisplayMetaName={props.airplaneDisplayMetaName} airplaneData={props.airplaneData} favoritePlanes={props.favoritePlanes} />
-      <RenderPlaneDetails airplaneDisplayMetaName={props.airplaneDisplayMetaName} metaData={metaData} airplaneData={props.airplaneData} favoritePlanes={props.favoritePlanes} />
-    </div>
-  )
-}
-
-function RenderPlaneDetails(props) {
-
-  let filteredMetaData = props.metaData.filter((item) => {
-    return item !== undefined
-  })
-
-  console.log(props.airplaneData);
-  console.log(filteredMetaData);
-
-  console.log(props.favoritePlanes);
-
-  let metaDetails = [];
-
-  let planeDetails = props.favoritePlanes.map((favPlanes) => {
-    metaDetails = [];
-    for (let planes of props.airplaneData) {
-      if (planes.icao.toLowerCase() === favPlanes) {
-        for (let meta of filteredMetaData) {
-          metaDetails.push(planes[meta]);
-        }
-        console.log(metaDetails);
-
-        return metaDetails;
-      }
-    }
-  })
-
-  console.log(planeDetails);
-
-  let data = planeDetails.map((arrayItem) => {
-    return arrayItem.map((planeItem) => {
-      console.log(planeItem);
-      let planeData = <p key={planeItem} className="chart-cell column">{planeItem}</p>
-      return planeData;
-    })
-  })
-
-  console.log(data);
-
-
-
-  // let data = for (let i = 0; i < planeDetails.length; i++) {
-  //   for (let j = 0; j < planeDetails[i].length; j++) {
-  //     console.log(planeData);
-  //     planeData = <p className="chart-cell column">{planeDetails[i][j]}</p>
-  //     return planeData;
-  //   }
-  // }
-
-  return (
-    <div>
-      {data}
-    </div>
-  )
-}
-
-
-function RenderPlaneName(props) {
-  let name;
-  let planeName = props.favoritePlanes.map((favPlane) => {
-    for (let i = 0; i < props.airplaneData.length; i++) {
-      if (props.airplaneData[i].icao.toLowerCase() === favPlane) {
-        name = <p className="chart-cell column" key={favPlane}>{props.airplaneData[i].make + " " + props.airplaneData[i].model}</p>
-        return name;
-      }
-    }
-  })
-
-  return (
-    <div>
-      {planeName}
-    </div>
-  )
-}
-
-function RenderPlaneImage(props) {
-  let image = props.favoritePlanes.map((favPlane) => {
-    for (let i = 0; i < props.airplaneData.length; i++) {
-      if (props.airplaneData[i].icao.toLowerCase() === favPlane) {
-        let imageSrc = "./plane-thumbnail/" + props.airplaneData[i].icao + ".jpg";
-        let imageAlt = "Picture of " + props.airplaneData[i].make + " " + props.airplaneData[i].model + " in " + props.airplaneData[i].make + " livery";
-
-        let image = <img key={favPlane} className="chart-cell column comparison-tile-image" src={imageSrc} alt={imageAlt} />
-        return image;
-      }
-    }
-  })
-
-  return (
-    <div>
-      {image}
-    </div>
-  )
-}
