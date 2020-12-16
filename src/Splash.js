e21655374d37c67306bcc8dbaa8bab1b726df5d1
David Xie <xdkdavid@gmail.com>
2020-12-15 18:43:39 -0800

    aesthetic improvement on first-loading splash page

--- src/Splash.js
+++ src/Splash.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Spinner } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './css/splash.css';
 
 export default function Splash() {
     return (
-        <div className="splash">
+        <div className="landing-splash-cotainer">
             <Spinner color="primary" className="splash-spinner" />
+            <h1 className='d-none d-sm-block'>Loading Airplane Data...</h1>
+            <p className='d-block d-sm-none'>Loading Airplane Data...</p>
         </div>
     );
 }
